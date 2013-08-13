1848708d7ba44b58674cc8abce38b5fa2faf67fa
Andrés Pesate <andrespesate@gmail.com>
2013-08-13 15:39:57 -0500

    Create Project
    I Create The Project

---
+++ AirHockey/AirHockey/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  AirHockey
+//
+//  Created by Andrés Pesate on 8/13/13.
+//  Copyright Andrés Pesate 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ AirHockey/AirHockey/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AirHockey
+//
+//  Created by Andrés Pesate on 8/13/13.
+//  Copyright Andrés Pesate 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
